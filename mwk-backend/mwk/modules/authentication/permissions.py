bb098b06cf417796b3a082c5d2f2279d87d4f8d8
Kento <46371150+kent7k@users.noreply.github.com>
2023-04-24 11:58:56 +0900

    Add Authentication App for Backend (#19)

---
+++ mwk-backend/mwk/modules/authentication/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsAnonymous(BasePermission):
+    """Allows access only to unauthenticated users"""
+
+    def has_permission(self, request, view):
+        return not request.user.is_authenticated
