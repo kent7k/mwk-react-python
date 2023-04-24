3268dae87cc6445531cb74517ff47b318036f9ee
kent7k <kent.ki720@gmail.com>
2023-04-24 13:37:58 +0900

    Add LocalStorage Libraries

---
+++ mwk-frontend/src/lib/index.tsx
@@ -0,0 +1,7 @@
+export * from './authentication'
+export * from './debounce'
+export * from './getFromLocalStorage'
+export * from './parseAPIAxiosErrors'
+export * from './removeFromLocalStorage'
+export * from './setToLocalStorage'
+export * from './showComponent'
---
+++ mwk-frontend/src/lib/setToLocalStorage/index.tsx
@@ -0,0 +1,2 @@
+export const setToLocalStorage = (key, value) =>
+  localStorage.setItem(key, value)
