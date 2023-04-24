7d7e7d1731d2ceb4023c5ce399c69ca0fa37717a
kent7k <kent.ki720@gmail.com>
2023-04-24 13:39:45 +0900

    Add FeedScroll

---
+++ mwk-frontend/src/components/feed/FeedInfiniteScroll/index.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useRef } from 'react'
+
+export const FeedInfiniteScroll = ({ onIntersecting }) => {
+  const loader = useRef(null)
+
+  const handleObserver = useCallback(
+    (entries) => {
+      const target = entries[0]
+      if (target.isIntersecting) {
+        onIntersecting()
+      }
+    },
+    [onIntersecting]
+  )
+
+  useEffect(() => {
+    const option = {
+      root: null,
+      rootMargin: '5px',
+      threshold: 1.0,
+    }
+    const observer = new IntersectionObserver(handleObserver, option)
+
+    if (loader.current) {
+      observer.observe(loader.current)
+    }
+
+    return () => {
+      observer.disconnect()
+    }
+  }, [handleObserver])
+
+  return <div ref={loader} />
+}
