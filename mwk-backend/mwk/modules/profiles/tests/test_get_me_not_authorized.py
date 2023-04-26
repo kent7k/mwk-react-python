cda25dfde290b9bf74d11f901d30f22e0c52e599
kent7k <kent.ki720@gmail.com>
2023-04-26 14:17:27 +0900

    a

--- mwk-backend/mwk/modules/profiles/tests/test_get_me_not_authorized.py
+++ mwk-backend/mwk/modules/profiles/tests/test_get_me_not_authorized.py
@@ -3,10 +3,7 @@ from datetime import datetime, timedelta
 from django.contrib.auth.models import User
 from django.urls import reverse
 from knox.models import AuthToken
-from rest_framework.settings import api_settings
 from rest_framework.test import APITestCase
-
-from mwk.modules.authentication.models import Profile
 
 
 class ProfileTests(APITestCase):
@@ -30,18 +27,6 @@ class ProfileTests(APITestCase):
         )
 
         self.token: str = AuthToken.objects.create(self.user)[-1]
-
-    def authenticate(self, token: str) -> None:
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token)
-
-    def setup_user_profile(self, user: User) -> Profile:
-        """Fill in the profile with test data"""
-
-        user.profile.birthday = self.birthday
-
-        user.profile.save()
-
-        return user.profile
 
     def test_get_me_not_authorized(self):
         """Test getting self profile unauthorized"""
