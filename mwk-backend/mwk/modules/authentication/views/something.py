514af8969206fa5909bc1b75aa685aba6b5f938a
Kento <46371150+kent7k@users.noreply.github.com>
2023-04-27 12:35:54 +0900

    Add admin.register (#78)

--- mwk-backend/mwk/modules/authentication/views/something.py
+++ mwk-backend/mwk/modules/authentication/views/something.py
@@ -1,19 +1,113 @@
-from typing import Type
+from typing import Union
 
-from rest_framework import generics
-from rest_framework.serializers import Serializer
+from django.contrib import admin, messages
+from django.core.exceptions import ValidationError
+from django.utils.safestring import SafeString, mark_safe
+from django.utils.translation import gettext as _
+from mptt.admin import MPTTModelAdmin
 
-from mwk.modules.authentication.permissions import IsAnonymous
-from mwk.modules.authentication.serializers.user_create_with_profile import UserCreateWithProfileSerializer
-from mwk.modules.authentication.services import send_activation_email
+from .admin_types import AdminModelForm
+from mwk.modules.main.models.comment import Comment
+from mwk.modules.main.models.image import Image
+from mwk.modules.main.models.post_category import PostCategory
+from mwk.modules.main.models.post import Post
 
 
-class UserRegisterAPIView(generics.CreateAPIView):
-    """Endpoint for user registration (create account)"""
+@admin.register(PostCategory)
+class PostCategoryAdmin(admin.ModelAdmin):
+    """PostCategory model admin"""
 
-    serializer_class = UserCreateWithProfileSerializer
-    permission_classes = (IsAnonymous,)
+    list_display = ['id', '__str__', 'created_at']
+    list_display_links = ['id', '__str__']
+    search_fields = ['id', 'title']
 
-    def perform_create(self, serializer: Type[Serializer]) -> None:
-        user = serializer.save()
-        send_activation_email(self.request, user)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post model admin"""
+
+    list_display = [
+        'id',
+        '__str__',
+        'author',
+        'category',
+        'liked_count',
+        'viewers_count',
+        'created_at',
+        'updated_at',
+        'get_post_photo',
+    ]
+    list_display_links = ['id', '__str__']
+    list_select_related = ['author', 'profile']
+    list_filter = ['category']
+    search_fields = ['id', 'title', 'author__username']
+    empty_value_display = '-'
+    autocomplete_fields = ['author', 'profile', 'category']
+    readonly_fields = [
+        'id',
+        'created_at',
+        'updated_at',
+        'viewers_count',
+        'liked_count',
+        'get_post_photo',
+    ]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('category', 'author').prefetch_related(
+            'liked', 'viewers', 'images'
+        )
+
+    def liked_count(self, obj: Post) -> int:
+        return obj.liked.count()
+
+    liked_count.short_description = 'Count of likes'
+
+    def viewers_count(self, obj: Post) -> int:
+        return obj.viewers.count()
+
+    viewers_count.short_description = 'Count of views'
+
+    def get_post_photo(self, obj: Post) -> Union[SafeString, str]:
+        images = obj.images
+
+        if images.exists():
+            image = images.first().photo
+            return mark_safe(f'<img src="{image.url}" height="40" width="40">')
+        else:
+            return '-'
+
+    get_post_photo.short_description = 'Thumbnail'
+
+
+@admin.register(Comment)
+class CommentAdmin(MPTTModelAdmin):
+    """Comment model mptt-admin"""
+
+    list_display = [
+        'id',
+        'post'[:50],
+        'author',
+        'liked_count',
+        'created_at',
+        'updated_at',
+        'is_reply',
+    ]
+    list_display_links = ['id', 'post']
+    search_fields = ['author__username', 'post__title', 'id']
+    empty_value_display = '-'
+    readonly_fields = ['id', 'liked_count', 'created_at', 'updated_at']
+    list_select_related = ['parent', 'author', 'post']
+    autocomplete_fields = ['author', 'post']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related('liked')
+
+    def is_reply(self, obj: Comment) -> str:
+        if not obj.parent:
+            return 'No'
+        else:
+            return 'Yes'
+
+
