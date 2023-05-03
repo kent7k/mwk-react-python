e6f8227979ecbbf057223f708d4b01136d75c9cc
kent7k <kent.ki720@gmail.com>
2023-05-03 20:54:33 +0900

    a

--- mwk-backend/mwk/modules/authentication/services/create_auth_token.py
+++ mwk-backend/mwk/modules/authentication/services/create_auth_token.py
@@ -1,24 +1,17 @@
-from datetime import datetime
-
 from django.contrib.auth.models import User
 from django.contrib.auth.signals import user_logged_in
 from django.utils import timezone
 from django.utils.translation import gettext as _
 from knox.models import AuthToken
-from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
 
-def create_auth_token(
-    request, user: User, token_limit_per_user: int, token_ttl: datetime
-) -> Response:
+def create_auth_token(request, user: User, token_limit_per_user: int = None, token_ttl: int = None):
     if token_limit_per_user is not None:
         now = timezone.now()
         token = user.auth_token_set.filter(expiry__gt=now)
         if token.count() >= token_limit_per_user:
-            return Response(
-                {'error': _('The maximum number of tokens per user has been reached.')},
-                status=403,
-            )
+            raise PermissionDenied(_('The maximum number of tokens per user has been reached.'))
 
     instance, token = AuthToken.objects.create(user, token_ttl)
     user_logged_in.send(sender=user.__class__, request=request, user=user)
