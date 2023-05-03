67bee9a0da9cf942d4f1d24584ebaa3478a28c82
Kento <46371150+kent7k@users.noreply.github.com>
2023-05-03 16:42:19 +0900

    a (#83)

---
+++ mwk-backend/mwk/modules/authentication/views/something.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in
+from django.utils import timezone
+from django.utils.translation import gettext as _
+from knox.models import AuthToken
+from rest_framework.response import Response
+
+
+def create_authtoken(request, user: User, token_limit_per_user: int, token_ttl: datetime) -> Response:
+    if token_limit_per_user is not None:
+        now = timezone.now()
+        if user.auth_token_set.filter(expiry__gt=now).count() >= token_limit_per_user:
+            return Response(
+                {'error': _('The maximum number of tokens per user has been reached.')},
+                status=403,
+            )
+
+    instance, token = AuthToken.objects.create(user, token_ttl)
+    user_logged_in.send(sender=user.__class__, request=request, user=user)
+    return request, token, instance
--- mwk-backend/mwk/modules/profiles/views/profile_detail_api_view.py
+++ mwk-backend/mwk/modules/profiles/views/profile_detail_api_view.py
@@ -1,6 +1,6 @@
 from rest_framework.generics import RetrieveAPIView, ListAPIView
 from mwk.modules.profiles.serializers.profile import ProfileSerializer
-from mwk.modules.profiles.services import get_profiles
+from mwk.modules.profiles.services.get_profiles import get_profiles
 
 
 class ProfileDetailView(RetrieveAPIView):
